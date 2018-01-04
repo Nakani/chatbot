28a2ec6861acfe2b0c0840221913a921e316039f
nakani <diogo.guedes.pereira@gmail.com>
2018-01-04 21:19:19 -0200

    ajuste

--- index.js
+++ index.js
@@ -19,10 +19,17 @@ restService.post("/echo", function(req, res) {
     req.body.result.parameters
       ? req.body.result.fulfillment.messages.speech
       : "Desculpa eu não entendi, pode repetir?";
+
+      // var opcoes = req.body.result.metadata.intentName;
+      // var filter = opcoes.split("-");
+      // switch(filterClean[1]){    
+      // }
+
   return res.json({
     speech: speech,
     displayText: speech,
-    source: "chatbot service pascal"
+    source: "chatbot service pascal",
+    partners:{}
   });
 });
 
