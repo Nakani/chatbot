6957d865efe195f7d81efed07bdfa2db819a68c7
Nakani <diogo.guedes.pereira@gmail.com>
2020-02-27 21:40:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-"use strict";
+const fs = require('fs');
+    const util = require('util');
+    const {struct} = require('pb-util');
+    // Imports the Dialogflow library
+    const dialogflow = require('dialogflow');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+    // Instantiates a session client
+    const sessionClient = new dialogflow.SessionsClient();
 
-const restService = express();
+    // The path to identify the agent that owns the created intent.
+    const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+    // Read the content of the audio file and send it as part of the request.
+    const readFile = util.promisify(fs.readFile);
+    const inputAudio = await readFile(filename);
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        audioConfig: {
+          audioEncoding: encoding,
+          sampleRateHertz: sampleRateHertz,
+          languageCode: languageCode,
+        },
+      },
+      inputAudio: inputAudio,
+    };
 
-restService.use(bodyParser.json());
+    // Recognizes the speech in the audio and detects its intent.
+    const [response] = await sessionClient.detectIntent(request);
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result && req.body.result.parameters
-      ? req.body.result.fulfillment.messages.speech
-      : "Desculpa eu não entendi, pode repetir?";
+    console.log('Detected intent:');
+    const result = response.queryResult;
+    // Instantiates a context client
+    const contextClient = new dialogflow.ContextsClient();
 
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "chatbot service pascal"
-  });
-});
-
-restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.oquebff.toLowerCase()) {
-    //Speech Synthesis Markup Language
-    case "musica um":
-      speech = "foi pedido musica um";
-      break;
-    case "musica dois":
-      speech = "foi pedido musica dois";
-      break;
-    case "musica tres":
-      speech = "foi pedido musica tres";
-      break;
-    case "bff":
-      speech = "Exemplo de uso com o bff";
-      break;
-  }
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
-});
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    const parameters = JSON.stringify(struct.decode(result.parameters));
+    console.log(`  Parameters: ${parameters}`);
+    if (result.outputContexts && result.outputContexts.length) {
+      console.log(`  Output contexts:`);
+      result.outputContexts.forEach(context => {
+        const contextId = contextClient.matchContextFromContextName(context.name);
+        const contextParameters = JSON.stringify(
+          struct.decode(context.parameters)
+        );
+        console.log(`    ${contextId}`);
+        console.log(`      lifespan: ${context.lifespanCount}`);
+        console.log(`      parameters: ${contextParameters}`);
+      });
+    }
